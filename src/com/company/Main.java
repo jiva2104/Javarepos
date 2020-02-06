2814fd94ee4ebc6e0feb7e8c5f8f041eac16a164
jiva2104 <jiva2104@gmail.com>
2020-02-06 11:50:53 +0200

    width=50 height=50

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("<p align=center><table width=250 height=250 border=1 bgcolor=\"#FKEEFF\"><tr><td align=center>THIS'S A TABLE</td></tr></table>");
+        System.out.println("<p align=center><table width=50 height=50 border=1 bgcolor=\"#FKEEFF\"><tr><td align=center>THIS'S A TABLE</td></tr></table>");
     }
 }

e7898262b7e849df1735ba4b170a1114a50c3661
jiva2104 <jiva2104@gmail.com>
2020-02-06 11:44:54 +0200

    width=250 height=250

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("<p align=center><table width=250 height=250 border=1 bgcolor=\"#FKEEFF\"><tr><td align=center>THIS'S A TABLE</td></tr></table>");
+    }
+}
